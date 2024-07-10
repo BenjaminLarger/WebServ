ca4486f244830f4d242acd400236d5f214dfda42
iisaacc <isporras@student.42malaga.com>
2024-07-10 19:41:18 +0200

    POST

--- inc/POST.hpp
+++ inc/POST.hpp
@@ -8,21 +8,22 @@
 class POST : public Webserv
 {
 
-private:
-  // Request line
-  std::string pathToRessource;
-  std::string HTTPversion;
-  // Header
-  std::string host;
-  std::string userAgent;
-  std::string accept;
-  // Body
-  std::string body;
+	private:
+		// Request line
+		std::istringstream	requestStream;
+		std::string			pathToRessource;
+		std::string			HTTPversion;
+		// Header
+		std::string			host;
+		std::string			userAgent;
+		std::string			accept;
+		// Body
+		std::string			body;
 
-public:
-  POST(Webserv _server, int serverFD, int clientFD, std::string &clientInput);
-  POST();
-  ~POST(void);
+	public:
+		POST(Webserv _server, int serverFD, int clientFD, std::string &clientInput);
+		POST();
+		~POST(void);
 
 };
 
--- src/POST.cpp
+++ src/POST.cpp
@@ -1,17 +1,26 @@
 #include "../inc/POST.hpp"
+#include "../inc/Dependencies.hpp"
 
-POST::POST(Webserv _server, int serverFD, int clientFD, std::string &clientInput) {
+POST::POST(Webserv _server, int serverFD, int clientFD, std::string &clientInput) : requestStream(clientInput) {
 
 
 	(void)_server;
 	(void)serverFD;
 	(void)clientFD;
 
-	std::istringstream isLine(clientInput);
-	std::string key;
+	std::string line;
 	std::cout << clientInput << std::endl;
 }
-
+	while (std::getline(this->requestStream, line) && line != "\r")
+	{
+		if (line.find("Content-Length:") != std::string::npos)
+		{
+			std::istringstream isLine(line);
+			std::string key;
+			isLine >> key;
+			isLine >> this->contentLength;
+		}
+	}
 POST::~POST(void) {
 
 }
