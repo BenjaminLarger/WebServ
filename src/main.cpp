7a717ac48d82953b35ab3b4b5d1e0462c8e0e6b4
Doruk Emre <doruk.emre@outlook.com>
2024-07-31 14:15:32 +0200

    Updated makefile and gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: demre <demre@student.42malaga.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 09:46:47 by blarger           #+#    #+#             */
-/*   Updated: 2024/07/30 15:35:48 by demre            ###   ########.fr       */
+/*   Updated: 2024/07/31 14:15:08 by demre            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int main(int argc, const char **argv)
   try
   {
     if (argc > 2)
-      throw(std::runtime_error("Too many arguments"));
+      throw(std::runtime_error(
+          "Too many arguments. Use: ./webserv [configuration file]"));
 
     std::string configFile = argv[1] ? argv[1] : DEFAULT_CONFIG_PATH;
     ServerConfig start(configFile.c_str());
